0508a9e810bd0ac20f5028aa5361a1b4f347e1a8
Taylor Chance <taylorchance@gmail.com>
2024-04-04 12:18:16 -0400

    attempt at hardcoding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-// google stream_id = '7811025791'
-// google measurement id = G-VPSEDVDH2C
-const GOOGLE_ANALYTICS_ID = 'G-VPSEDVDH2C'
+// google stream_id = '7813857982'
+// google measurement id = G-GHD0MMW7FD
+// const GOOGLE_ANALYTICS_ID = 'G-GHD0MMW7FD'
 
 export default defineNuxtConfig({
   app: {
     head: {
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-GHD0MMW7FD', async: true },
+        { src: 'js/ga.js' }
+    ],
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
@@ -15,16 +19,16 @@ export default defineNuxtConfig({
     },
   },
   css: ['~/assets/css/bulma.css'],
-  modules: [
-    '@nuxtjs/google-analytics'
-  ],
-  googleAnalytics: {
-    id: GOOGLE_ANALYTICS_ID,
-  },
-  publicRuntimeConfig: {
-    googleAnalytics: {
-      id: GOOGLE_ANALYTICS_ID
-    }
-  },
+  // modules: [
+  //   '@nuxtjs/google-analytics'
+  // ],
+  // googleAnalytics: {
+  //   id: GOOGLE_ANALYTICS_ID,
+  // },
+  // publicRuntimeConfig: {
+  //   googleAnalytics: {
+  //     id: GOOGLE_ANALYTICS_ID
+  //   }
+  // },
   devtools: { enabled: true },
 })
