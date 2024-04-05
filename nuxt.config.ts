c0af5215fb0ee6a4c838230e13afbe6e160499d7
Taylor Chance <taylorchance@gmail.com>
2024-04-05 16:58:02 -0400

    meta and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,17 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      title: 'LCBO Aeroplan points maximizer',
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet' },
       ],
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'description', content: 'LCBO Aeroplan points maximizer' }
+      ]
     },
   },
   css: ['~/assets/css/bulma.css', '~/assets/css/bulma-list.css'],
