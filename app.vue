d0a9422aa520e80ab55869cd320d8396c3225235
Taylor Chance <taylorchance@gmail.com>
2024-04-05 17:06:05 -0400

    icon

--- app.vue
+++ app.vue
@@ -137,7 +137,15 @@ const formatDate = (string) => {
               <span class="has-text-weight-light"> / {{ product.points }}</span>
               <span class="pts">pts</span>
             </p>
-            <a :href="product.url" target="_blank" class="has-text-dark is-size-6">View LCBO product</a>
+            <a :href="product.url" target="_blank" class="help">
+              <span>View LCBO product</span>
+              <span class="icon is-small" style="position: relative; top: 2px; left: 4px;">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                  <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
+                  <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
+                </svg>
+              </span>
+            </a>
           </div>
 
           <div class="list-item-controls has-text-centered">
